07d939fec13ee00440a7ff4470df9e8e82d21086
Pranav Balu <51266456+pranavbalu1@users.noreply.github.com>
2024-09-27 12:00:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ def clone_image(input_image_path, output_image_path):
 def modify_pixel(pixel):
     r, g, b = pixel
     
-    #add lsb insertion here
-    new_r = r
-    new_g = g
-    new_b = b
+    #add lsb insertion here, for not just inverting the colors
+    new_r = 255 - r
+    new_g = 255 - g
+    new_b = 255 - b
     
     return (new_r, new_g, new_b)
 
